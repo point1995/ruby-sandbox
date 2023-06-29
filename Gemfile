2a9bea13948867c909908551b26a3e2c2a59e95c
point1995 <128720525+point1995@users.noreply.github.com>
2023-06-29 04:30:50 +0000

    homework

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "active_support"
+gem "awesome_print"
+gem "pry-byebug"
+
---
+++ howdy.rb
@@ -0,0 +1,8 @@
+require "active_support/all"
+require "./goodbye.rb"
+
+
+#my_string = "Howdy, partner!"
+#pp my_string
+
+pp 1.ordinalize
